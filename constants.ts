3de8715ba9f898b509b30b9103cddeffc3c80a5c
Saad <saad@contco.org>
2021-04-27 13:59:55 -0400

    fixes

---
+++ constants.ts
@@ -0,0 +1,5 @@
+export const TERRA_TEST_NET = { URL: 'https://tequila-lcd.terra.dev', chainID: 'tequila-0004' };
+export const TERRA_MAIN_NET = { URL: 'https://tequila-lcd.terra.dev', chainID: 'tequila-0004' };
+
+//temp
+export const IS_TEST = true;
--- pages/api/terra-core/core.ts
+++ pages/api/terra-core/core.ts
@@ -1,8 +1,7 @@
 import { LCDClient, Coin } from '@terra-money/terra.js';
+import { IS_TEST, TERRA_TEST_NET, TERRA_MAIN_NET } from '../../../constants';
 
-
-const terra = new LCDClient({ URL: 'https://tequila-lcd.terra.dev', chainID: 'tequila-0004' });
-
+const terra = new LCDClient(IS_TEST ? TERRA_TEST_NET : TERRA_MAIN_NET);
 
 export const getBankBalance = async ({ args: { address } }: any) => {
     const balance = await terra.bank.balance(address);
