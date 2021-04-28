b41c85b628675a3c2c418dcf558dfb15619f87b7
Hamza Alam <getright_02@live.com>
2021-04-28 23:44:52 +0500

    refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import Header from '../components/Header/Header';
+import Header from '../components/Header';
 
 
 const Home: React.FC = ({ theme, changeTheme }: any) => {
