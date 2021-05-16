84eba3233ee992f9fde9f2332555dcf0c9cff67c
Saad <saad@contco.org>
2021-05-15 21:13:51 -0400

    assets

--- pages/api/terra-core/core.ts
+++ pages/api/terra-core/core.ts
@@ -6,5 +6,5 @@ const terra = new LCDClient(IS_TEST ? TERRA_TEST_NET : TERRA_MAIN_NET);
 export const getBankBalance = async ({ args: { address } }: any) => {
     const balance = await terra.bank.balance(address);
     const coins = balance.toData();
-    return coins;
+    return { address, tokens: coins };
 }
--- pages/api/terra-core/index.ts
+++ pages/api/terra-core/index.ts
@@ -3,20 +3,30 @@ import { buildFederatedSchema } from '@apollo/federation';
 import { getBankBalance } from './core';
 
 const typeDefs = gql`
-    type Coin {
+    type Token {
         denom: String
         amount: String
     }
 
+    type Assets @key(fields: "address") {
+        address: String!
+        tokens: [Token]
+    }
+
     type Query {
-        getBankBalance(address: String!): [Coin]
+        assets(address: String!): Assets
     }
 `;
 
 const resolvers = {
     Query: {
-        getBankBalance(parent, args, context) { return getBankBalance({ args }) },
+        assets(parent, args, context) { return getBankBalance({ args }) },
     },
+    Assets: {
+        __resolveReference(assets) {
+            return getBankBalance({ args: { address: assets.address } })
+        }
+    }
 };
 
 const apolloServer = new ApolloServer({ schema: buildFederatedSchema([{ typeDefs, resolvers }]) });
