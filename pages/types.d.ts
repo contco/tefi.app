53aabe0a8719d5374ea92033f2568756d983693f
Fahad-Mahmood <fahad492@outlook.com>
2021-06-18 04:24:25 +0500

    anchor/mirror data ts types added

---
+++ pages/types.d.ts
@@ -0,0 +1,110 @@
+interface Token {
+  __typename?: 'Token'
+  symbol: string
+  amount: string
+  price: string
+  staked?: string
+};
+
+interface Reward  {
+  __typename?: 'Reward'
+  name?: string
+  staked?: string
+  apy?: string
+  reward?: string
+};
+
+interface Airdrops {
+  __typename?: 'Airdrops'
+  price?: string
+  quantity?: string
+  round?: number
+  name?: string
+};
+
+interface UserCollateral {
+  __typename?: 'UserCollateral'
+  collateral?: string
+  balance?: string
+};
+
+interface BorrowData  {
+  __typename?: 'BorrowData'
+  reward?: Reward
+  limit: string
+  value?: string
+  collaterals: Array<UserCollateral>
+};
+
+interface EarnData  {
+  __typename?: 'EarnData'
+  reward?: Reward
+};
+
+interface LpData {
+  __typename?: 'LPData'
+  reward: Reward
+  balance: string
+};
+
+interface GovData {
+  __typename?: 'GovData'
+  reward: Reward
+};
+
+interface Total {
+  __typename?: 'Total'
+  airdropSum: string
+};
+
+interface AccountAnc  {
+  __typename?: 'AccountANC'
+  assets?: Array<Token>
+  debt?: BorrowData
+  earn?: EarnData
+  pool?: LpData
+  gov?: GovData
+  airdrops?: Array<Airdrops>
+  total?: Total
+};
+
+
+interface MirrorAccountAssets {
+    __typename?: 'AccountAssets'
+    symbol: string,
+    apr: string,
+    unstakedToken?: string
+    unstakedUstValue?: string
+    ustStaked?: string
+    tokenStaked?: string
+    tokenStakedUstValue?: string
+    stakeTotalUstValue?: string
+    poolTotalWithRewards?: string
+    rewards?: string
+    rewardsUstValue?: string
+    price?: string
+    name?: string
+    lpBalance?: string
+  }
+
+  interface AssetsTotal {
+    __typename?: 'AssetsTotal'
+    rewardsSum: string
+    stakedSum: string
+    unstakedSum: string
+    airdropSum: string
+  }
+
+interface MirrorAccount {
+    __typename?: 'Account'
+    assets: Array<MirrorAccountAssets>
+    total: AssetsTotal
+    airdrops: Array<Airdrops>
+  }
+
+interface Assets {
+  __typename?: 'Assets'
+  address: string
+  anchor?: AccountAnc
+  mirror?: MirrorAccount
+};
