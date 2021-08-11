864f4ed5087780d873505d0c9a893540e400447f
Saad <saad@contco.org>
2021-08-11 17:49:49 -0400

    fix

--- constants.ts
+++ constants.ts
@@ -42,3 +42,5 @@ export enum WalletConnectType {
   Mobile,
 }
 
+export const TEFI_PREVIEW_IMAGE = 'https://storage.googleapis.com/tefi-app/tefi-preview.png';
+export const TEFI_API_PREVIEW_IMAGE = 'https://storage.googleapis.com/tefi-app/tefi-app-api-preview.png';
