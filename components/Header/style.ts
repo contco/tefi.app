37b00a109450a36874142e7c1edb171594637f66
Fahad-Mahmood <fahad492@outlook.com>
2021-08-28 08:32:17 +0500

    view only dashboard page added

--- components/Header/style.ts
+++ components/Header/style.ts
@@ -117,10 +117,11 @@ transition: all 0.3s ease-in;
 })}
 `;
 export const CloseIcon = styled(CLOSE_ICON)`
-${css({
+${props => css({
   transform: ['scale(0.5)', null, null, null, 'scale(0.5)'],
   width: 30,
-  color: 'secondary'
+  color: 'secondary',
+  visibility: props.showIcon ? 'visible' : 'hidden',
 })}
 transition: all 0.3s ease-in;
   &:hover {
@@ -150,7 +151,7 @@ ${props => css({
   border: `solid 2px ${props.theme.colors.secondary}`,
   mr: [2, 2, null, 4],
   cursor: 'pointer',
-  justifyContent: 'space-between',
+  justifyContent: 'space-around',
   alignItems: 'center',
   textAlign: 'center',
   p: 1,
