20f51f8b56145ef748f053262fde855d00f785a6
Hamza Alam <getright_02@live.com>
2021-09-07 02:17:03 +0500

    ui

---
+++ pages/transactions/index.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Header from '../../components/Header';
+import styled from 'styled-components';
+
+const EmptyContainer = styled.div`
+  height: 100vh;
+  width: 100vh;
+  background-color: ${(props) => props.theme.colors.background};
+`;
+
+const Home: React.FC = ({ theme, changeTheme }: any) => {
+  const [isDisplay, setIsDisplay] = useState<boolean>(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsDisplay(false);
+    setTimeout(() => setIsDisplay(true), 1000);
+  }, [router.pathname]);
+
+  return (
+    <div>
+      {!isDisplay ? (
+        <EmptyContainer />
+      ) : (
+        <div>
+          <div>
+            <Header theme={theme} changeTheme={changeTheme} />
+          </div>
+
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
