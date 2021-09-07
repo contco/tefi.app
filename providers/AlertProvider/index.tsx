4fe8087f7bc8ce949d33f61490cb08458d968ff2
Fahad-Mahmood <fahad492@outlook.com>
2021-09-07 15:16:04 +0500

    alert message updated

--- providers/AlertProvider/index.tsx
+++ providers/AlertProvider/index.tsx
@@ -4,6 +4,8 @@ import { useAssetPriceContext } from '../AssetPriceProvider';
 import { askNotificationPermission } from './helpers';
 
 const ALERT_KEY = 'alerts';
+const CROSSING_UP = 'Up';
+const CROSSING_DOWN = 'Down';
 
 interface AlertContextProps{
     alerts: Alerts;
@@ -51,11 +53,11 @@ const AlertProvider: React.FC<Props> = ({ children }) => {
     localStorage.setItem(ALERT_KEY, JSON.stringify(newAlerts));
   };
 
-  const displayPriceNotification = (price, symbol) => {
+  const displayPriceNotification = (price: string, symbol: string, direction: string) => {
      if (Notification.permission === 'granted') {
       audio.play();
       const image = window.location.origin + '/tefi.jpg';
-      const text = `${symbol.toUpperCase()} Crossing ${price}`;
+      const text = `${symbol.toUpperCase()} Crossing ${direction} ${price}`;
       const notification = new Notification('Tefi Price Alert', { body: text , image});
       const url = '/market/' + symbol;
       notification.onclick = () => {
@@ -70,13 +72,13 @@ const AlertProvider: React.FC<Props> = ({ children }) => {
   useEffect(() => {
    Object.keys(realTimePrices).map((key: string) => {
       if(+realTimePrices[key] < +alerts[key]?.price  && alerts[key]?.priceDirection !== PriceDirection.down) {
-        displayPriceNotification(alerts[key].price, key);
+        displayPriceNotification(alerts[key].price, key, CROSSING_DOWN);
         const newAlerts = {...alerts, [key]: {...alerts[key], priceDirection: PriceDirection.down}};
         setAlerts(newAlerts);
         updateLocalStorageState(newAlerts);
       }
       else if (+realTimePrices[key] > +alerts[key]?.price && alerts[key]?.priceDirection !== PriceDirection.up) {
-        displayPriceNotification(alerts[key].price, key);
+        displayPriceNotification(alerts[key].price, key, CROSSING_UP);
         const newAlerts = {...alerts, [key]: {...alerts[key], priceDirection: PriceDirection.up}};
         setAlerts(newAlerts);
         updateLocalStorageState(newAlerts);
@@ -87,7 +89,7 @@ const AlertProvider: React.FC<Props> = ({ children }) => {
 
   const setPriceAlert = async (symbol: string, price: string, currentPrice: string) => {
     currentPrice = realTimePrices[symbol] ? realTimePrices[symbol] : currentPrice;
-    const currentDirection = +currentPrice > +price ? PriceDirection.up : PriceDirection.down;
+    const currentDirection = parseFloat(currentPrice) > parseFloat(price) ? PriceDirection.up : PriceDirection.down;
     const newAlerts = {...alerts, [symbol]: {price, priceDirection: currentDirection}};
     setAlerts(newAlerts);
     updateLocalStorageState(newAlerts);
