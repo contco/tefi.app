7485ba31f5cc72dc1813662f4b479eb0d0d082dd
billalkhn <billalkhn98@gmail.com>
2021-09-27 23:28:19 +0500

    Lota Rewards Calculate successfully

--- pages/api/loterra/lib/staking.ts
+++ pages/api/loterra/lib/staking.ts
@@ -2,6 +2,7 @@ import { getPoolInfo, wasmStoreRequest } from '../../commons';
 import { contracts } from './contracts';
 import { getLpStakingInfo } from './getLpStaking';
 import { getGovInfo } from './getGovInfo';
+import { UNIT } from '../../mirror/utils';
 
 export const getLoterraStaking = async (address: string) => {
   try {
@@ -24,26 +25,28 @@ export const getLoterraStaking = async (address: string) => {
         address: address,
       },
     };
+    const accrued_rewards = {
+      accrued_rewards: {
+        address: address,
+      },
+    };
 
     const poolInfoRequest = getPoolInfo(contracts.pool);
     const holderRequest = wasmStoreRequest(contracts.staking, holderMsg);
     const claimsRequest = wasmStoreRequest(contracts.staking, claimMsg);
     const lpTokenRequest = wasmStoreRequest(contracts.loterraLPAddress, LpTokenMsg);
     const holderLP = wasmStoreRequest(contracts.loterraStakingLPAddress, holderLPMsg);
+    const LPHolderAccruedRewards = wasmStoreRequest(contracts.loterraStakingLPAddress, accrued_rewards);
 
-    const [
-      poolInfo,
-      holderInfo,
-      claimInfo,
-      lpTokenInfo,
-      holderLPInfo
-    ] = await Promise.all([
+    const [poolInfo, holderInfo, claimInfo, lpTokenInfo, holderLPInfo, lpRewardsInfo] = await Promise.all([
       poolInfoRequest,
       holderRequest,
       claimsRequest,
       lpTokenRequest,
-      holderLP
+      holderLP,
+      LPHolderAccruedRewards,
     ]);
+    console.log('Rewards: ', lpRewardsInfo.rewards  / UNIT);
 
     const lotaPool = getLpStakingInfo(poolInfo, lpTokenInfo, holderLPInfo);
     const lotaGov = getGovInfo(holderInfo, poolInfo, claimInfo);
