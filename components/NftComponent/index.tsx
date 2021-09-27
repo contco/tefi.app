33d7b766b89dcda8a8d5a073cfc82f5b48ba573e
Ammar Jussa <ammar26497@gmail.com>
2021-09-27 23:29:57 +0500

    Animation added

--- components/NftComponent/index.tsx
+++ components/NftComponent/index.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import css from '@styled-system/css';
 import { Text, Flex, Box } from '@contco/core-ui';
+import { SmallCircle } from './animatingCircle';
 
 const MainContainer = styled(Flex)`
   justify-content: center;
@@ -9,6 +10,7 @@ const MainContainer = styled(Flex)`
     height: ['calc(100vh - 320px)'],
   })}
 `;
+
 const Container = styled(Box)`
   align-items: center;
   justify-content: center;
@@ -16,7 +18,7 @@ const Container = styled(Box)`
 
 const BigCircle = styled(Flex)`
   border-radius: 50%;
-  align-items: flex-end;
+  align-items: flex-start;
   justify-content: center;
   margin: 0 auto;
   ${css({
@@ -26,15 +28,6 @@ const BigCircle = styled(Flex)`
   })};
 `;
 
-const SmallCircle = styled(Flex)`
-  border-radius: 50%;
-  ${css({
-    height: ['30px', null,'40px'],
-    width: ['30px', null,'40px'],
-    bg: 'primary',
-  })}
-`;
-
 const BigText = styled(Text)`
   font-weight: 900;
   letter-spacing: 0.77px;
@@ -42,6 +35,7 @@ const BigText = styled(Text)`
   ${css({
     fontSize: ['30px', null, '40px', '60px', null, null, null, '80px'],
     mt: [8],
+    color: 'Heading',
   })}
 `;
 
