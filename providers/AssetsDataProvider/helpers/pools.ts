b1d299bac1ab8deefff53b984639b01edffe9bac
billalkhn <billalkhn98@gmail.com>
2021-09-30 10:08:57 +0500

    Pool and Rewards section

--- providers/AssetsDataProvider/helpers/pools.ts
+++ providers/AssetsDataProvider/helpers/pools.ts
@@ -1,20 +1,26 @@
 import { parse } from 'graphql';
 import { convertToFloatValue } from '../../../utils/convertFloat';
 
-export const getPoolData = (anchor, mirror, pylon, terraSwap  , loterra) => {
-  console.log(loterra)
+export const getPoolData = (anchor, mirror, pylon, terraSwap, loterra) => {
+  console.log(loterra);
   const getPoolTotal = () => {
     const pylonPoolTotal = pylon?.pylonSum?.pylonPoolSum;
     const total =
       parseFloat(pylonPoolTotal) +
       parseFloat(mirror?.total?.mirrorPoolSum) +
       parseFloat(anchor?.total?.anchorPoolSum) +
-      parseFloat(terraSwap.total)+
-       parseFloat(loterra.lotaPool.totalLpUstValue)
+      parseFloat(terraSwap.total) +
+      parseFloat(loterra.lotaPool.totalLpUstValue);
     return total.toString() ?? '0';
   };
 
-  const pool = [...pylon?.pylonPool, ...mirror?.mirrorStaking, ...anchor.pool, ...terraSwap.list ].sort(
+  const pool = [
+    ...pylon?.pylonPool,
+    ...mirror?.mirrorStaking,
+    ...anchor.pool,
+    ...terraSwap.list,
+    loterra.lotaPool,
+  ].sort(
     (a, b) =>
       parseFloat(b.stakeableLpUstValue) +
       parseFloat(b.stakedLpUstValue) -
