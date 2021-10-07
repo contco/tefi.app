634e20cd187c246e4e49a83c9af601f79a29b6ed
Saad <saad@contco.org>
2021-10-07 03:09:43 -0400

    fix

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -67,7 +67,7 @@ const Header: React.FC<Props> = ({ theme, changeTheme, address, addressType, onR
       <Container>
         <LeftSection>
           <StyledTitle onClick={() => router.push('/')}>
-            Anon<span>{logoSub}</span>
+            YourNameHere<span>{logoSub}</span>
           </StyledTitle>
         </LeftSection>
         <RightSection>
