72cc44428799796a3e97b48469916a4d3db7e516
Saad <saad@contco.org>
2021-10-18 12:53:03 -0400

    add logo back

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -67,7 +67,7 @@ const Header: React.FC<Props> = ({ theme, changeTheme, address, addressType, onR
       <Container>
         <LeftSection>
           <StyledTitle onClick={() => router.push('/')}>
-            <span>{""}</span>
+            Tefi<span>{"App"}</span>
           </StyledTitle>
         </LeftSection>
         <RightSection>
