88ccd4547a8000df0235d0c40fce407069835b32
Fahad-Mahmood <fahad492@outlook.com>
2022-08-03 19:29:08 +0500

    feat: agora home responsive styles added

--- components/Agora/Sidebar/index.tsx
+++ components/Agora/Sidebar/index.tsx
@@ -17,9 +17,8 @@ const AgoraTitle = styled(Text)`
   })}
 `;
 
-
 const Container = styled(Box)`
-box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 2px 0px;
+  box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 2px 0px;
   ${css({
     p: 3,
     width: 400,
@@ -29,29 +28,35 @@ box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 2px 0px;
 `;
 
 const SidebarItem = styled(Text)`
-  ${props => css({
-    p: 4,
-    fontSize: 2,
-    fontWeight: 'bold',
-    color: 'detailsText',
-    cursor: 'pointer',
-    bg: props.isSelected ? 'hoverBackground' : 'primary',
-    '&:hover': {
+  ${(props) =>
+    css({
+      p: 4,
+      fontSize: 2,
+      fontWeight: 'bold',
+      color: 'detailsText',
+      cursor: 'pointer',
+      bg: props.isSelected ? 'hoverBackground' : 'primary',
+      '&:hover': {
         bg: 'hoverBackground',
-    },
-    mt: 1,
-    transition: '0.3s',
+      },
+      mt: 1,
+      transition: '0.3s',
+    })}
+`;
 
-  })}
-`
-
-export const Sidebar = ({selectedCategory, setCategory}) => {
+export const Sidebar = ({ selectedCategory, setCategory }) => {
   return (
     <Container>
       <AgoraTitle>{AGORA_TITLE}</AgoraTitle>
-        {THREAD_CATEGORIES.map(category => (
-            <SidebarItem onClick={() => setCategory(category.category)} isSelected={selectedCategory === category.category} key={category.id}>{category.category}</SidebarItem>
-        ))} 
+      {THREAD_CATEGORIES.map((category) => (
+        <SidebarItem
+          onClick={() => setCategory(category.category)}
+          isSelected={selectedCategory === category.category}
+          key={category.id}
+        >
+          {category.category}
+        </SidebarItem>
+      ))}
     </Container>
   );
 };
