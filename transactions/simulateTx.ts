bd40e8ec49e0a6353fc6563f4d5d83c209c76b2c
Fahad-Mahmood <fahad492@outlook.com>
2022-08-23 14:10:03 +0500

    fix: calculateFee removed, simulateTc updated

--- transactions/simulateTx.ts
+++ transactions/simulateTx.ts
@@ -12,15 +12,20 @@ export const simulateTx = async (
   isTestnet: boolean,
   gasAdjustment = DEFAULT_GAS_ADJUSTMENT,
 ) => {
-  const txOptions = { msgs, feeDenoms: [denom], memo, gasAdjustment };
-  const gasPrices = { [denom]: gasPrice };
-  const lcd = isTestnet
-    ? new LCDClient({ chainID: networks.testnet.chainID, URL: networks.testnet.lcd, gasPrices })
-    : new LCDClient({ chainID: networks.mainnet.chainID, URL: networks.mainnet.lcd, gasPrices });
+  try {
+    const txOptions = { msgs, feeDenoms: [denom], memo, gasAdjustment };
+    const gasPrices = { [denom]: gasPrice };
+    const lcd = isTestnet
+      ? new LCDClient({ chainID: networks.testnet.chainID, URL: networks.testnet.lcd, gasPrices })
+      : new LCDClient({ chainID: networks.mainnet.chainID, URL: networks.mainnet.lcd, gasPrices });
 
-  const unsignedTx = await lcd.tx.create([{ address }], txOptions);
-  const estimatedFee = unsignedTx.auth_info.fee.toAmino();
-  const gas = estimatedFee.gas;
-  const amount = estimatedFee.amount[0].amount;
-  return { unsignedTx, gas, estimatedFee: amount };
+    const unsignedTx = await lcd.tx.create([{ address }], txOptions);
+    const estimatedFee = unsignedTx.auth_info.fee;
+    const estimatedFeeInAmino = estimatedFee.toAmino();
+    const gas = estimatedFeeInAmino.gas;
+    const amount = estimatedFeeInAmino.amount[0].amount;
+    return { unsignedTx, gas, estimatedFee: amount, fee: estimatedFee };
+  } catch (err) {
+    return { error: true, msg: 'Error calculating fee' };
+  }
 };
